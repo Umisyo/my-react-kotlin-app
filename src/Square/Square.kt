8122542c92d9824773805d94fd48c63bb66cc3fd
Umisyo <kusunokisouta@gmail.com>
2019-06-27 19:11:36 +0900

    'initial commit'

---
+++ src/Square/Square.kt
@@ -0,0 +1,15 @@
+package Square
+
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+import react.dom.button
+
+class Square (): RComponent<RProps, RState>() {
+    override fun RBuilder.render() {
+        button(classes = "square") { /*TODO*/ }
+    }
+}
+
+fun RBuilder.square() = child(Square::class) {}
---
+++ src/main/main.kt
@@ -0,0 +1,15 @@
+package main
+
+import Game.game
+import react.dom.render
+import kotlin.browser.document
+import kotlin.browser.window
+
+fun main(args: Array<String>) {
+    window.onload = {
+        val root = document.getElementById("root") ?: throw IllegalStateException()
+        render(root) {
+            game()
+        }
+    }
+}
