1565362c226e2fd4a910f3f7ef0cdf4174f8e23b
Umisyo <kusunokisouta@gmail.com>
2019-06-29 03:54:13 +0900

    'リフトアップ、再び'

--- src/Board/Board.kt
+++ src/Board/Board.kt
@@ -6,62 +6,15 @@ import react.RComponent
 import react.RProps
 import react.RState
 import react.dom.div
-import react.setState
 
-class Board: RComponent<RProps, Board.State>() {
-    init {
-        state.squares = Array(9) { null }
-        state.xIsNext = true
-    }
-
+class Board: RComponent<Board.Props, RState>() {
     private fun RBuilder.renderSquare(i: Int) {
-        square(value = state.squares[i],
-                onClickFunction = { handleClick(i) })
-    }
-
-    private fun handleClick(i: Int): Unit? {
-        val updateSquares = state.squares.copyOf()
-
-        if (calculateWinner(updateSquares) != null || updateSquares[i] != null) return null
-
-        if (state.xIsNext) updateSquares[i] = "X"
-        else updateSquares[i] = "O"
-
-        return setState {
-            squares = updateSquares
-            xIsNext = !state.xIsNext
-        }
-    }
-
-    private fun calculateWinner(squares:Array<String?>): String? {
-        val lines: Array<Array<Int>> = arrayOf(
-                arrayOf(0, 1, 2),
-                arrayOf(3, 4, 5),
-                arrayOf(6, 7, 8),
-                arrayOf(0, 3, 6),
-                arrayOf(1, 4, 7),
-                arrayOf(2, 5, 8),
-                arrayOf(0, 4, 8),
-                arrayOf(2, 4, 6)
-        )
-
-        for (line in lines) {
-            val (a, b, c) = line
-            if (squares[a] != null && squares[a] == squares[b] && squares[a] == squares[c]) {
-                return squares[a]
-            }
-        }
-
-        return null
-
+        square(value = props.squares[i],
+                onClickFunction = { props.onClickFunction(i) })
     }
 
     override fun RBuilder.render() {
-        val winner: String? = calculateWinner(state.squares)
-        val status = if(winner != null) "Winner: $winner" else ("Next player :" + (if (state.xIsNext) "X" else "O"))
-
         div {
-            div(classes = "status") { + status }
             div(classes = "board-row") {
                 renderSquare(0)
                 renderSquare(1)
@@ -80,11 +33,13 @@ class Board: RComponent<RProps, Board.State>() {
         }
     }
 
-
-    interface State: RState {
+    interface Props: RProps {
         var squares: Array<String?>
-        var xIsNext: Boolean
+        var onClickFunction: (Int) -> Unit
     }
 }
 
-fun RBuilder.board() = child(Board::class) {}
+fun RBuilder.board(squares:Array<String?>, onClickFunction: (Int) -> Unit) = child(Board::class) {
+    attrs.squares = squares
+    attrs.onClickFunction = onClickFunction
+}
--- src/Game/Game.kt
+++ src/Game/Game.kt
@@ -6,15 +6,78 @@ import react.RComponent
 import react.RProps
 import react.RState
 import react.dom.div
+import kotlin.arrayOf as arrayOf1
+import react.setState
 
-class Game(): RComponent<RProps, RState>() {
+class Game: RComponent<RProps, Game.State>() {
+    init {
+       state.apply {
+           history = arrayOf1(HistoryEntry(Array(9) { null }))
+           xIsNext = true
+       }
+    }
+
+    interface State: RState {
+        var history: Array<HistoryEntry>
+        var xIsNext: Boolean
+    }
+
+    data class HistoryEntry(var squares: Array<String?>)
+
+    fun handleClick(i: Int){
+        val history = state.history
+        val current= state.history.last()
+        val squares = current.squares.copyOf()
+
+        if (calculateWinner(squares) != null || squares[i] != null) return
+
+        squares[i] = if(state.xIsNext) "X" else "O"
+
+        setState{
+            this.history = history + HistoryEntry(squares)
+            xIsNext = !state.xIsNext
+        }
+    }
+
+    private fun calculateWinner(squares:Array<String?>): String? {
+        val lines: Array<Array<Int>> = arrayOf1(
+                arrayOf1(0, 1, 2),
+                arrayOf1(3, 4, 5),
+                arrayOf1(6, 7, 8),
+                arrayOf1(0, 3, 6),
+                arrayOf1(1, 4, 7),
+                arrayOf1(2, 5, 8),
+                arrayOf1(0, 4, 8),
+                arrayOf1(2, 4, 6)
+        )
+
+        for (line in lines) {
+            val (a, b, c) = line
+            if (squares[a] != null && squares[a] == squares[b] && squares[a] == squares[c]) {
+                return squares[a]
+            }
+        }
+        return null
+    }
+
     override fun RBuilder.render() {
+        val history = state.history
+        val current = state.history.last()
+        val winner = calculateWinner(current.squares)
+        val status: String =
+                when {
+                    winner != null -> "Winner: $winner"
+                    else -> "Next player: ${if (state.xIsNext) "X" else "O"}"
+                }
+
         div(classes = "game") {
             div(classes = "game-board") {
-                board()
+                board(current.squares){
+                    handleClick(it)
+                }
             }
             div(classes = "game-info") {
-                div {/*status*/}
+                div { + status}
                 div{/*TODO*/}
             }
         }
