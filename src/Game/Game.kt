a6211775a99f669f09b0abccb8aa9f42840e5f32
Umisyo <kusunokisouta@gmail.com>
2019-06-29 13:10:46 +0900

    '完成'

--- src/Game/Game.kt
+++ src/Game/Game.kt
@@ -1,32 +1,38 @@
 package Game
 
 import Board.board
+import kotlinx.html.js.onClickFunction
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
+import react.dom.button
 import react.dom.div
-import kotlin.arrayOf as arrayOf1
+import react.dom.li
+import react.dom.ol
+import kotlin.arrayOf
 import react.setState
 
 class Game: RComponent<RProps, Game.State>() {
     init {
        state.apply {
-           history = arrayOf1(HistoryEntry(Array(9) { null }))
+           history = arrayOf(HistoryEntry(Array(9) { null }))
            xIsNext = true
+           stepNumber = 0
        }
     }
 
     interface State: RState {
         var history: Array<HistoryEntry>
         var xIsNext: Boolean
+        var stepNumber: Int
     }
 
     data class HistoryEntry(var squares: Array<String?>)
 
-    fun handleClick(i: Int){
-        val history = state.history
-        val current= state.history.last()
+    private fun handleClick(i: Int){
+        val history = state.history.sliceArray(0..state.stepNumber)
+        val current= history.last()
         val squares = current.squares.copyOf()
 
         if (calculateWinner(squares) != null || squares[i] != null) return
@@ -36,19 +42,20 @@ class Game: RComponent<RProps, Game.State>() {
         setState{
             this.history = history + HistoryEntry(squares)
             xIsNext = !state.xIsNext
+            stepNumber = history.size
         }
     }
 
     private fun calculateWinner(squares:Array<String?>): String? {
-        val lines: Array<Array<Int>> = arrayOf1(
-                arrayOf1(0, 1, 2),
-                arrayOf1(3, 4, 5),
-                arrayOf1(6, 7, 8),
-                arrayOf1(0, 3, 6),
-                arrayOf1(1, 4, 7),
-                arrayOf1(2, 5, 8),
-                arrayOf1(0, 4, 8),
-                arrayOf1(2, 4, 6)
+        val lines: Array<Array<Int>> = arrayOf(
+                arrayOf(0, 1, 2),
+                arrayOf(3, 4, 5),
+                arrayOf(6, 7, 8),
+                arrayOf(0, 3, 6),
+                arrayOf(1, 4, 7),
+                arrayOf(2, 5, 8),
+                arrayOf(0, 4, 8),
+                arrayOf(2, 4, 6)
         )
 
         for (line in lines) {
@@ -60,9 +67,32 @@ class Game: RComponent<RProps, Game.State>() {
         return null
     }
 
+    private fun jumpTo (step: Int) {
+        setState{
+            stepNumber = step
+            xIsNext = (step % 2) == 0
+        }
+    }
+
+    private fun RBuilder.moves () =
+        state.history.mapIndexed{ step, _ ->
+            val desc = when {
+                step != 0 -> "Go to move #$step"
+                else -> "Go to game start"
+            }
+
+            li {
+                button {
+                    + desc
+                    attrs.onClickFunction = { jumpTo(step) }
+                }
+            }
+        }
+
+
     override fun RBuilder.render() {
         val history = state.history
-        val current = state.history.last()
+        val current = history[state.stepNumber]
         val winner = calculateWinner(current.squares)
         val status: String =
                 when {
@@ -78,7 +108,7 @@ class Game: RComponent<RProps, Game.State>() {
             }
             div(classes = "game-info") {
                 div { + status}
-                div{/*TODO*/}
+                ol{ moves ()}
             }
         }
     }
